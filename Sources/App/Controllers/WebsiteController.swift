b6fa921ae030e90137aad07e59b638fdb49a3066
c <cmhunlu@yahoo.com>
2020-04-19 19:24:00 +0300

    added create leaf

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -3,8 +3,12 @@ import Vapor
 struct WebsiteController: RouteCollection {
     func boot(router: Router) throws {
         router.get(use: indexHandler)
-        
         router.get("airplanes",AirPlane.parameter, use: airplaneHandler)
+        router.get("aircompanies",AirCompany.parameter,use: aircompanyHandler)
+        router.get("aircompanies", use: allAircompanyHandler)
+        router.get("airplanes","create",use: createAirplaneHandler)
+        router.post(AirPlane.self,at: "airplane","create", use: createAirplanePostHandler)
+        
         
         
      
@@ -17,13 +21,7 @@ struct WebsiteController: RouteCollection {
         }
         
     }
-//    func airportHandler(_ req: Request) throws -> Future<View> {
-//        return AirPort.query(on: req).all().flatMap(to: View.self) { airport in
-//            let context = AirportContext(title: "Airports", airport: airport.isEmpty ? nil : airport)
-//            return try req.view().render("airports",context)
-//            
-//        }
-//    }
+
    
     func airplaneHandler(_ req: Request) throws -> Future<View> {
         return try req.parameters.next(AirPlane.self).flatMap(to: View.self) { airplane in
@@ -31,6 +29,31 @@ struct WebsiteController: RouteCollection {
                 let context = AirplaneContext(title: airplane.brand, airplane: airplane, aircompany: aircompany)
                 return try req.view().render("airplanes",context)
             }
+        }
+    }
+    func aircompanyHandler(_ req: Request) throws -> Future<View> {
+        return try req.parameters.next(AirCompany.self).flatMap(to: View.self) { aircompany in
+            let context = try AircompanyContext(title: aircompany.companyCountry, aircompany: aircompany, airplanes: aircompany.airplane.query(on: req).all())
+            return try req.view().render("aircompany",context)
+        }
+        
+    }
+    func allAircompanyHandler(_ req: Request) throws -> Future<View> {
+        let context = AllAircompanyContext(aircompanies: AirCompany.query(on: req).all())
+        return try req.view().render("allAircompanies", context)
+    }
+
+    func createAirplaneHandler(_ req: Request) throws -> Future<View> {
+        let context = CreateAirplaneContext(aircompanies: AirCompany.query(on: req).all())
+        return try req.view().render("createAirplane", context)
+    }
+    func createAirplanePostHandler(_ req: Request, airplane: AirPlane) throws -> Future<Response> {
+        return airplane.save(on: req).map(to: Response.self) { airplane in
+            guard let id = airplane.id else {
+                return req.redirect(to: "/")
+            }
+            return req.redirect(to: "/airplanes/\(id)")
+            
         }
     }
     
@@ -42,17 +65,25 @@ struct IndexContext : Encodable {
     
 }
 
-
-//struct AirportContext : Encodable {
-//    let title: String
-//    let airport: [AirPort]?
-//}
-
 struct AirplaneContext: Encodable {
     let title: String
     let airplane: AirPlane
     let aircompany: AirCompany
 }
 
+struct AircompanyContext : Encodable {
+    let title: String
+    let aircompany : AirCompany
+    let airplanes : Future<[AirPlane]>
+}
+
+struct AllAircompanyContext : Encodable {
+    let title = "All AirCompany"
+    let aircompanies: Future<[AirCompany]>
+}
 
 
+struct CreateAirplaneContext: Encodable {
+    let title = "Create An Airplane"
+    let aircompanies : Future<[AirCompany]>
+}
